4bf4106e8ae8755b9143b4f39676ce453c56f4be
torychow <522617505@qq.com>
2016-12-03 12:44:44 +0800

    Spring boot

---
+++ demo/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,19 @@
+/**	______	    		
+   /__	__/__  ____     
+ 	 / / / _ \/ __) _ `/
+ 	/_/ ()___/_/  \_, /
+ 				 /___/			
+ * 2016年12月3日 上午9:33:32
+ */
+package com.example.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+	
+	public String getUser(){
+		return "User got";
+	}
+}
