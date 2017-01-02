3aa78b0832373ee32691b960e285e836676e86fa
toryzhou <522617505@qq.com>
2017-01-02 21:26:41 +0800

    User Controller

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
 	
 	@GetMapping("user")
 	public String getUser(){
-		System.out.println("in the controller  ---====");
+		System.out.println("in the UserController  ---====");
 		return userService.getUser();
 	}
 
